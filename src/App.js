e9a60a03e1b5d8bf36d951d7b9ea7e9a690ea2c4
Andrew King <kinga0879@gmail.com>
2022-07-10 23:15:18 -0400

    buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Stack} from '@mui/material'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Select your tool
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Stack spacing={2}>
+        <Button href="http://10.0.1.179:9117" variant='contained'> Jacket</Button>
+        <Button href="http://10.0.1.179:8080" variant='contained'> Flood Torrent</Button>
+        <Button href="https://10.0.1.179:9443" variant='contained'> Portainer</Button> 
+    </Stack>
+  
       </header>
     </div>
   );
