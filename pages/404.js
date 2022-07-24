fa8c5bc68e6a0b1639a7f345ac2f21359fa2a840
Andrew King <kinga0879@gmail.com>
2022-07-24 12:44:15 -0400

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Typography } from "@material-ui/core";
+import Image from 'next/image'
+const useStyles = makeStyles(typographyStyle);
+
+export default function _error (props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+
+  const mainContent = <></>
+
+  const topContent = <>
+  <Box sx={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'}}>
+    <Typography className={classes.stupidHeading} >404 horse not found</Typography>
+    <Image width='100px' height='100px' src='/img/shorse.png' />
+    
+  </Box></>
+
+  return (
+    <>Typography>404040404044</Typography>
+  );
+}
