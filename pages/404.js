8b39dcb500fb6494e8727f3c3e8b373c343f899b
Andrew King <kinga0879@gmail.com>
2022-07-24 12:46:23 -0400

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,12 @@
 import React from "react";
 
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography } from "@mui/core";
 import Image from 'next/image'
 
 export default function _error (props) {
   
   const { ...rest } = props;
 
-  const mainContent = <></>
-
-  const topContent = <>
-  <Box sx={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'}}>
-    <Typography className={classes.stupidHeading} >404 horse not found</Typography>
-    <Image width='100px' height='100px' src='/img/shorse.png' />
-    
-  </Box></>
 
   return (
     <Typography>404040404044</Typography>
